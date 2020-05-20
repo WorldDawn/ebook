449425cda8b237f00ad73d36a7003649e5dff4ac
WorldDawn <394641620@qq.com>
2020-05-20 15:12:21 +0800

    第三章 项目准备

---
+++ test/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{welcome}}</h1>
+</template>
+
+<script>
+  export default{
+	data(){
+		return{
+			welcome: 'Hello Vue-cli!'
+		}
+	}
+  }
+</script>
---
+++ vue-ebook/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <span class="text">ABCDEFG</span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  const getters = {
+    a: () => 1,
+    b: () => 2,
+    c: () => 3
+  }
+  function fn(keys) {
+    const data = {}
+    keys.forEach(key => {
+      // eslint-disable-next-line no-prototype-builtins
+      if (getters.hasOwnProperty(key)) {
+        data[key] = getters[key]
+      }
+    })
+    return data
+  }
+
+  export default {
+    computed: {
+      ...mapGetters(['test']),
+      ...fn(['a', 'b', 'c'])
+    },
+    mounted() {
+      // this.$store.dispatch('setTest', 9).then(() => {
+      //   console.log(this.test)
+      // })
+      console.log(this.a, this.b, this.c)
+    }
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
+
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
+  .text{
+    font-family: "Days One";
+    font-size: px2rem(20);
+    color: #ffa500;
+  }
+</style>
